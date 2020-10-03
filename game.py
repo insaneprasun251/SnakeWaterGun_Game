64ff44e1ba7acb150ad05e7cbb5820dfb0811577
insaneprasun251 <72152148+insaneprasun251@users.noreply.github.com>
2020-10-03 17:35:24 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 #snake water gun game :
 #first we need random module :
 #comparision between random and player :
-print("     ~ WELCOME TO SNAKE WATER GUN GAME ~     ")
+print("            ~ WELCOME TO SNAKE WATER GUN GAME ~     ")
 import random
 dice =["snake","water","gun"]
 c=0
@@ -12,53 +12,53 @@ hl=0
 draw=0
 while c!=10:
     robot = random.choice(dice)
-    print("------- ROUND",c+1,"-------")
-    player = input("Enter your choice from s,w,g...\n ")
+    print("                  ------- ROUND",c+1,"-------")
+    player = input("Enter your choice from S,W,G...\n ")
     if robot == "snake" and player.lower()== "s":
-        print('Tie both 0 pointS to each')
+        print('Tie both will get 0 Poins')
         draw+=1
         c+=1
     elif robot == "snake" and player.lower() == "w":
         cw+=1
-        print('you loose computer win\nyour point is',hw,'and computer point is',cw)
+        print('You loose computer win\nyour point is',hw,'and computer point is',cw)
         c+=1
     elif robot == "snake" and player.lower() == "g":
         hw+=1
-        print('you win computer loose\nyour point is',hw,'and computer point is',cw)
+        print('You win computer loose\nyour point is',hw,'and computer point is',cw)
         c+=1
     elif robot == "water" and player.lower() == "s":
         hw+=1
-        print('you win computer loose\nyour point is',hw,'and computer point is',cw)
+        print('You win computer loose\nyour point is',hw,'and computer point is',cw)
         c+=1
     elif robot == "water" and player.lower() == "g":
         hw+=1
-        print('you win computer loose\nyour point is',hw,'and computer point is',cw)
+        print('You win computer loose\nyour point is',hw,'and computer point is',cw)
         c+=1
     elif robot == "water" and player.lower() == "w":
         draw+=1
-        print('Tie both 0 point to each')
+        print('Tie both will get 0 Points')
         c+=1
     elif robot == 'gun' and player.lower() == "g":
         draw+=1
-        print('Tie both 0 point to each')
+        print('Tie both will get 0 Points')
         c+=1
     elif robot == "gun" and player.lower() == 'w':
         cw+=1
-        print('you loose computer win\nyour point is',hw,'and computer point is',cw)
+        print('You loose computer win\nyour point is',hw,'and computer point is',cw)
         c+=1
     elif robot == 'gun' and player.lower() == 's':
         cw+=1
-        print('you loose computer win\nyour point is',hw,'and computer point is',cw)
+        print('You loose computer win\nyour point is',hw,'and computer point is',cw)
         c+=1
     else:
-        print("wrong input")
+        print("Wrong input")
     print(10-c,'rounds left')
 else:
     if hw>cw:
-        print('you won the game')
+        print('             ~~~~~~~ You won the game ~~~~~~~')
     elif hw<cw:
-        print('you loose the game')
+        print('             ~~~~~~~ You loose the game ~~~~~~~')
     else:
-        print('game draw')
-input('press enter to exit')
+        print('             ~~~~~~~ Game draw ~~~~~~~')
+input('Press enter to Exit')
 
